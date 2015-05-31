415c9e8291871cd8619ad3f533a6fcfffd65f9d0
Lee Fallat <me@leefallat.ca>
2015-05-31 11:53:01 +0000

    ONE statistics on front page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -9,9 +9,17 @@ class HomeController extends BaseController {
 	 */
 	public function home() {
         $posts = Post::ViewableTo(Auth::user())->orderBy('created_at', 'desc')->paginate(10); 
-        
+	    
+	    
+	    $lastMember = DB::select("SELECT id FROM users ORDER BY id DESC LIMIT 1;")[0]->id;
+	    $lastPost = DB::select("SELECT id FROM posts ORDER BY id DESC LIMIT 1;")[0]->id;
+	    $lastComment = DB::select("SELECT id FROM comments ORDER BY id DESC LIMIT 1;")[0]->id;
+	    
 		return View::make('home/home')
 			->with('post', new Post())
+			->with('lastMember',$lastMember)
+			->with('lastPost',$lastPost)
+			->with('lastComment',$lastComment)
 			->withPosts($posts);
 	}
 
